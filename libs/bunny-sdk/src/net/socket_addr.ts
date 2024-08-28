ec2d60695b7369464403fe5184792d7031654d33
Anthony Griffon <anthony+w1@griffon.one>
2024-08-28 10:15:57 +0200

    feat: Add no addr option

--- libs/bunny-sdk/src/net/socket_addr.ts
+++ libs/bunny-sdk/src/net/socket_addr.ts
@@ -1,21 +1,27 @@
 import { IPv4 } from "./ip.ts";
 
-export type SocketAddr = {
+export type SocketAddr = SocketAddrV4 | NoAddr;
+
+export type SocketAddrV4 = {
   readonly _tag: "SocketAddrV4",
   port: number,
   ip: IPv4,
 };
 
+export type NoAddr = {
+  readonly _tag: "NoAddr",
+};
+
 /**
   * Returns the port number associated with this socket address.
   */
-export function port(addr: SocketAddr): number {
+export function port(addr: SocketAddrV4): number {
   return addr.port;
 }
 
 /**
   * Returns the IP address associated with this socket address.
   */
-export function ip(addr: SocketAddr): IPv4 {
+export function ip(addr: SocketAddrV4): IPv4 {
   return addr.ip;
 }
--- libs/bunny-sdk/src/net/tcp.ts
+++ libs/bunny-sdk/src/net/tcp.ts
@@ -1,3 +1,4 @@
+import { internal_getPlatform } from "../platform.ts";
 import * as SocketAddr from "./socket_addr.ts";
 
 export type TcpListener = {
@@ -24,20 +25,38 @@ export function unstable_local_addr(tcp: TcpListener): SocketAddr.SocketAddr {
   * Format the associated [TcpListener] to a String
   */
 export function toString(tcp: TcpListener): string {
-  return `${SocketAddr.ip(tcp.addr).join('.')}:${SocketAddr.port(tcp.addr)}`;
+  switch (tcp.addr._tag) {
+    case "SocketAddrV4":
+      return `${SocketAddr.ip(tcp.addr).join('.')}:${SocketAddr.port(tcp.addr)}`;
+    case "NoAddr":
+      return "No addr associated to this listener. You should be running in a controlled environment.";
+  }
 }
 
 /**
   * Create a new [TcpListener].
   */
 export function unstable_new(): TcpListener {
-  return ({
-    _tag: 'TcpListener',
-    addr: {
-      _tag: "SocketAddrV4",
-      port: 8080,
-      ip: [127, 0, 0, 1],
-    },
-  })
+  const platform = internal_getPlatform();
+  switch (platform.runtime) {
+    case "bunny": {
+      return ({
+        _tag: "TcpListener",
+        addr: {
+          _tag: "NoAddr"
+        }
+      });
+    }
+    default:
+      return ({
+        _tag: 'TcpListener',
+        addr: {
+          _tag: "SocketAddrV4",
+          port: 8080,
+          ip: [127, 0, 0, 1],
+        },
+      });
+
+  }
 }
 
